d8e37e63293b4c6b34884ae92da25065d9c9a87e
defnotad <aditya.arya.ggn@gmail.com>
2020-07-12 21:12:28 +0530

    Animation and callbacks

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var documents = document.querySelectorAll(".drum");
-var audio = new Audio("sounds/tom-1.mp3");
+var audio;
 
-document.addEventListener("keypress", function (event) {
-    switch (event.key) {
+function buttonAnimation(currentKey) {
+    var active = document.querySelector("." + currentKey);
+    active.classList.add("pressed");
+    setTimeout(function () { active.classList.remove("pressed"); }, 100);
+}
+
+function checker(checker) {
+    switch (checker) {
         case "w":
             audio = new Audio("sounds/crash.mp3");
             break;
@@ -28,36 +34,16 @@ document.addEventListener("keypress", function (event) {
             break;
     }
     audio.play();
+}
+
+document.addEventListener("keypress", function (event) {
+    checker(event.key);
+    buttonAnimation(event.key);
 });
 
 for (var i = 0; i < documents.length; i++) {
     documents[i].addEventListener("click", function () {
-        var audio;
-        switch (this.innerText) {
-            case "w":
-                audio = new Audio("sounds/crash.mp3");
-                break;
-            case "a":
-                audio = new Audio("sounds/kick-bass.mp3");
-                break;
-            case "s":
-                audio = new Audio("sounds/snare.mp3");
-                break;
-            case "d":
-                audio = new Audio("sounds/tom-1.mp3");
-                break;
-            case "j":
-                audio = new Audio("sounds/tom-2.mp3");
-                break;
-            case "k":
-                audio = new Audio("sounds/tom-3.mp3");
-                break;
-            case "l":
-                audio = new Audio("sounds/tom-4.mp3");
-                break;
-            default:
-                break;
-        }
-        audio.play();
+        checker(this.innerText);
+        buttonAnimation(this.innerText);
     });
 }
